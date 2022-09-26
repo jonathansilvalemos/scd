22c00443fb5566349af024bf67d5aef110d254a4
Jonathan Lemos <sbtdobrasiloficial@gmail.com>
2022-09-26 18:31:02 -0300

    Project created

---
+++ backend/src/main/java/com/jonathanlemos/scd/ScdApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanlemos.scd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jonathanlemos/scd/ScdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanlemos.scd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
