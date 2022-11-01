d11cac8d3c503fffaf8f5c8951bf7b91c12b78c8
Jonathan Lemos <sbtdobrasiloficial@gmail.com>
2022-11-01 19:40:52 -0300

    Cadastro link5

--- backend/src/main/java/com/jonathanlemos/scd/controllers/UsuarioController.java
+++ backend/src/main/java/com/jonathanlemos/scd/controllers/UsuarioController.java
@@ -43,6 +43,7 @@ public class UsuarioController {
 	}
 	
 	@PutMapping
+	@ResponseBody
 	public UsuarioDTO atualizarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
 		return usuarioService.atualizarUsuario(usuarioDTO);
 	}
