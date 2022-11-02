f7af953691bc7c39b78ba66dce127a77575a5e08
Jonathan Lemos <sbtdobrasiloficial@gmail.com>
2022-11-02 00:55:34 -0300

    autenticacao2

--- backend/src/main/java/com/jonathanlemos/scd/controllers/UsuarioController.java
+++ backend/src/main/java/com/jonathanlemos/scd/controllers/UsuarioController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- backend/src/main/java/com/jonathanlemos/scd/services/UsuarioService.java
+++ backend/src/main/java/com/jonathanlemos/scd/services/UsuarioService.java
@@ -17,27 +17,27 @@ import com.jonathanlemos.scd.repositories.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private PasswordEncoder encoder;
-	
+
 	@Transactional(readOnly = true)
 	public Page<UsuarioDTO> buscarTodos(Pageable pageable) {
-		Page<Usuario> usuario = usuarioRepository.findAll(PageRequest.of(0,30,Sort.by(Sort.Direction.ASC, "codigo"))); 
+		Page<Usuario> usuario = usuarioRepository.findAll(PageRequest.of(0, 30, Sort.by(Sort.Direction.ASC, "codigo")));
 		Page<UsuarioDTO> page = usuario.map(x -> new UsuarioDTO(x));
 		return page;
 	}
-	
+
 	@Transactional(readOnly = true)
 	public UsuarioDTO findById(Long id) {
-		Usuario result = usuarioRepository.findById(id).get(); 
+		Usuario result = usuarioRepository.findById(id).get();
 		UsuarioDTO dto = new UsuarioDTO(result);
 		return dto;
 	}
-	
+
 	@Transactional
 	public UsuarioDTO atualizarUsuario(UsuarioDTO usuarioDTO) {
 		Usuario user = new Usuario();
@@ -49,7 +49,7 @@ public class UsuarioService {
 		user = usuarioRepository.saveAndFlush(user);
 		return new UsuarioDTO(user);
 	}
-	
+
 	@Transactional
 	public Optional<UsuarioDTO> login(Long matricula, String senha) {
 		Optional<Usuario> usuario = usuarioRepository.findByMatricula(matricula);
@@ -63,13 +63,13 @@ public class UsuarioService {
 				user.setNome(usuario.get().getNome());
 				user.setSenha(usuario.get().getSenha());
 				user.setTipo(usuario.get().getTipo());
-				
+
 				return Optional.of(user);
 			}
 		}
 		return null;
 	}
-	
+
 	@Transactional
 	public Optional<UsuarioDTO> findByMatricula(Long matricula) {
 		Optional<Usuario> usuario = usuarioRepository.findByMatricula(matricula);
@@ -79,25 +79,23 @@ public class UsuarioService {
 		user.setNome(usuario.get().getNome());
 		user.setSenha(usuario.get().getSenha());
 		user.setTipo(usuario.get().getTipo());
-		
+
 		return Optional.of(user);
 	}
-	
+
 	@Transactional
 	public UsuarioDTO salvarUsuario(UsuarioDTO usuarioDTO) {
 		Optional<Usuario> usuario = usuarioRepository.findByMatricula(usuarioDTO.getMatricula());
-		
+
 		if (usuario.isEmpty()) {
-			System.out.println("eh vazio");
 			Usuario usuarioCodificado = new Usuario();
 			usuarioCodificado.setMatricula(usuarioDTO.getMatricula());
 			usuarioCodificado.setNome(usuarioDTO.getNome());
 			usuarioCodificado.setSenha(encoder.encode(usuarioDTO.getSenha()));
 			usuarioCodificado.setTipo(usuarioDTO.getTipo());
-			usuarioCodificado = usuarioRepository.saveAndFlush(usuarioCodificado);
+			usuarioCodificado = usuarioRepository.save(usuarioCodificado);
 			return new UsuarioDTO(usuarioCodificado);
 		}
-		
 		return null;
 	}
 }
