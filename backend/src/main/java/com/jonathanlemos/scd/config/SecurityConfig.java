9ad322e1f7432869a2a7c788d0d2b74fc78a4aa3
Jonathan Lemos <sbtdobrasiloficial@gmail.com>
2022-12-15 00:02:29 -0300

    ultimos testes3

--- backend/src/main/java/com/jonathanlemos/scd/config/SecurityConfig.java
+++ backend/src/main/java/com/jonathanlemos/scd/config/SecurityConfig.java
@@ -1,36 +1,29 @@
 package com.jonathanlemos.scd.config;
 
 import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-	@Autowired
-	private Environment env;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
-			http.headers().frameOptions().disable();
-		}
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
+		http.headers().frameOptions().disable();
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+
+		return http.build();
 	}
 
 	@Bean
