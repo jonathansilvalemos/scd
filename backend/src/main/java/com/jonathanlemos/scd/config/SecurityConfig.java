88f6d5742b22f7e32bf93d721a38aab4b51001d9
Jonathan Lemos <sbtdobrasiloficial@gmail.com>
2022-12-14 23:57:27 -0300

    ultimos testes2

--- backend/src/main/java/com/jonathanlemos/scd/config/SecurityConfig.java
+++ backend/src/main/java/com/jonathanlemos/scd/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
 			http.headers().frameOptions().disable();
 		}
 		
