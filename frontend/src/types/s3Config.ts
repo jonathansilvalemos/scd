f7143d5fcec273923e7d42cb4cb34095e31911e6
jonathansilvalemos <90199884+jonathansilvalemos@users.noreply.github.com>
2022-12-16 23:56:31 -0300

    tirando aws

--- frontend/src/types/s3Config.ts
+++
@@ -1,9 +0,0 @@
-export const s3Config = {
-    bucketName:  'scdescala',
-    dirName: 'escala', 
-    region: 'sa-east-1',
-    accessKeyId:'AKIAW5NT6UCC4CIR7QTG',
-    secretAccessKey: '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',
-    s3Url:'https://scdescala.s3.sa-east-1.amazonaws.com/escala/'
-}
-
